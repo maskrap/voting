1f70eb3620128ead2220480533dfdb6b9b004e54
Rachael Bermudes and Sam Park <sam@>
2016-06-02 08:42:13 -0700

    adding branches and html

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  var age = parseInt(prompt("How old are you?"));
+
+  if (age >= 18) {
+    $('#candidates').show();
+  } else {
+    $('#under-18').show();
+  }
+  event.preventDefault();
+});
